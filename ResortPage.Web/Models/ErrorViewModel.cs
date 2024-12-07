84f00b0bb6c31f74fce146c2145533d7116b542c
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-12-07 12:42:41 -0500

    Add project files.

---
+++ ResortPage.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResortPage.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
